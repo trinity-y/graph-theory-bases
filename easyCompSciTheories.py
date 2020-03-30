8a1a0f17aefa211bad1d68f780e1a444334ef0f3
unknown <trinity-y>
2020-03-30 15:05:12 -0400

    added project

---
+++ easyCompSciTheories.py
@@ -0,0 +1,26 @@
+#prefix sum base
+original = list(map(int, input().split()))#takes a list of integers 
+prefixsumarray = []#creates an array for the prefix sum
+prefixsumarray[0] = original[0]#start off the array before the loop
+for i in range(len(original)):#every time
+  prefixsumarray[i] = prefixsumarray[i-1] + original[i]#int is the previous int plus the num from original
+def getSum(a, b):#sum equals
+  prefixsumarray[a] - prefixsumarray[b] + original[b]
+
+#power recursion example
+def power(base, exponent):
+	#BASE CASE: x^0 = 1
+	if exponent == 0:
+		return 1
+	#BODY: manipulate variable from base
+	else:
+		return base * power(base, exponent - 1)
+
+#/*adjacency list base IN C#*/ 
+
+
+#adjacency matrix base
+
+#depth for search base
+
+
---
+++ graphTheoryBases.py
@@ -0,0 +1,33 @@
+'''
+adjacency matrix base
+'''
+# #graphData[0] = # of nodes; graph[1] = # of edges
+print("input # of nodes followed by # of edges")
+graphData = list(map(int, input("").split()))
+adjMatrix = [[0 for i in range(graphData[0] + 1)] for i in range(graphData[0] + 1)]#creates a graphData[0] x graphData[0] (not including 0 indexes) matrix filled with 0s
+print("input node neighbour pairs")
+for i in range(graphData[1]):
+	neighbours = list(map(int, input().split()))
+	nodeA = neighbours[0]
+	nodeB = neighbours[1]
+	adjMatrix[nodeA][nodeB] = 1
+	adjMatrix[nodeB][nodeA] = 1
+print('\n'.join([' '.join(str(row)) for row in adjMatrix]))
+
+'''
+adjacency list base
+'''
+#graphData[0] = # of nodes; graph[1] = # of edges
+print("input # of nodes followed by # of edges")
+graphData = list(map(int, input().split()))
+adjList = [[] for i in range(graphData[0] + 1)]
+print("input node neighbour pairs")
+for i in range(graphData[1]):
+	neighbourPair = list(map(int, input().split()))
+	nodeA = neighbourPair[0]
+	nodeB = neighbourPair[1]
+	adjList[nodeA].append(nodeB)
+	adjList[nodeB].append(nodeA)
+print(adjList)
+
+
